25b61400c38013fc751fbb4a3e6a9be9f6435d50
Justin Stigall <jstigall@gmail.com>
2023-03-01 20:17:09 -0600

    tailwind css and luxon

--- app/src/Fastier.tsx
+++ app/src/Fastier.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DateTime } from 'luxon';
 
 const Fastier = () => {
+	const [fastStartTime, setFastStartTime] = useState<DateTime>();
+
 	return (
-		<div id="fastier">
-			<h1>Hello World!</h1>
+		<div id="fastier" className='bg-slate-500'>
+			<h1 className='text-3xl font-bold underline'>Fastier</h1>
+			{fastStartTime !== undefined && <><h2>Your Fast started at {fastStartTime.toLocaleString(DateTime.DATETIME_FULL)}</h2>
+				<h2>Your 18 hour fast will end at {fastStartTime.plus({ hours: 18 }).toLocaleString(DateTime.DATETIME_FULL)}</h2></>}
+			<button onClick={() => setFastStartTime(DateTime.now())}>Start Fasting</button>
 		</div>
 	);
 };
