27bda29eebc01bcd8faec7d2238d56c9080ceccb
Justin Stigall <jstigall@gmail.com>
2023-03-04 11:18:32 -0600

    countdown style changes

--- app/src/FastCountdown.tsx
+++ app/src/FastCountdown.tsx
@@ -2,13 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { DateTime, Duration } from 'luxon';
 
 const FastCountdown = () => {
-	const [fastStartTime, setFastStartTime] = useState<DateTime>();
 	const [timeLeft, setTimeLeft] = useState<Duration>(Duration.fromMillis(0));
 
+	const [stateData, setStateData] = useState({
+		fastStart: '2023-03-03T17:27:16.663-06:00',
+		fastDuration: 18,
+	});
+
+	const setFastStartTime = (d: DateTime) => {
+		const temp = stateData;
+		temp.fastStart = d.toISO();
+		setStateData(temp);
+	};
+
 	const calcTimeLeft = () => {
-		if (fastStartTime !== undefined) {
-			setTimeLeft(fastStartTime.plus({ hours: 18 }).diffNow());
+		if (stateData.fastStart !== undefined) {
+			setTimeLeft(DateTime.fromISO(stateData.fastStart).plus({ hours: 18 }).diffNow());
 		}
+	};
+
+	const percentLeft = () => {
+		return 100 - ((timeLeft.milliseconds / (stateData.fastDuration * 60 * 60 * 1000)) * 100);
 	};
 
 	useEffect(() => {
@@ -20,11 +34,19 @@ const FastCountdown = () => {
 	});
 
 	return (
-		<div id="fastCountdown">
-			{timeLeft.toFormat('hh:mm:ss')}
-			{fastStartTime !== undefined && <><h2>Your Fast started at {fastStartTime.toLocaleString(DateTime.DATETIME_FULL)}</h2>
-				<h2>Your 18 hour fast will end at {fastStartTime.plus({ hours: 18 }).toLocaleString(DateTime.DATETIME_FULL)}</h2></>}
-			<button type="button" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setFastStartTime(DateTime.now())}>Start Fasting</button>
+		<div id="fastCountdown" className='grid grid-cols-1 place-items-center px-10'>
+			<div className='text-2xl'>{timeLeft.toFormat('hh:mm:ss')}</div>
+			<div className='w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
+				<div className='bg-blue-600 h-2.5 rounded-full' style={{ width: percentLeft() + '%' }}></div>
+			</div>
+
+			{stateData.fastStart !== undefined && <><h2>Started: {DateTime.fromISO(stateData.fastStart).toLocaleString(DateTime.DATETIME_FULL)}</h2>
+				<h2>Ends at: {DateTime.fromISO(stateData.fastStart).plus({ hours: 18 }).toLocaleString(DateTime.DATETIME_FULL)}</h2></>}
+			<div>
+				<button type="button" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 mx-1 rounded' onClick={() => setFastStartTime(DateTime.now())}>⏱️ Start</button>
+				<button type="button" className='bg-red-400 hover:bg-red-700 text-white font-bold py-1 px-2 mx-1 rounded'>🛑 Stop</button>
+				<button type="button" className='bg-gray-800 hover:bg-gray-700 text-white font-bold py-1 px-2 mx-1 rounded'>✏️ Edit</button>
+			</div>
 		</div>
 	);
 };
